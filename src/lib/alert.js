2ad59f5689ce73442ebd96aef78ccad305e9e306
jefrialbukhori <bangjeff200505@gmail.com>
2025-12-05 23:16:07 +0700

    Add files via upload

---
+++ src/lib/alert.js
@@ -0,0 +1,35 @@
+import Swal from 'sweetalert2'
+
+export const alertSuccess = async (message) => {
+    return Swal.fire({
+        icon: 'success',
+        title: 'Success!',
+        text: message,
+    })
+}
+
+export const alertError = async (message) => {
+    return Swal.fire({
+        icon: 'error',
+        title: 'Error!',
+        text: message,
+    })
+}
+
+export const alertConfirm = async (message) => {
+    const result = await Swal.fire({
+        icon: 'question',
+        title: 'Are you sure?',
+        text: message,
+        background: '#FFEBD3',
+        color: '#226B80',
+
+        showCancelButton: true,
+        confirmButtonColor: '#226B80',
+        cancelButtonColor: '#da373d',
+
+        confirmButtonText: 'Yes',
+        cancelButtonText: 'Cancel',
+    })
+    return result.isConfirmed;
+}
